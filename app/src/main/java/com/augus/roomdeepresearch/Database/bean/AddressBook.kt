bd7b35bb486da54de5a3da4f6536d921363da915
augus_0331 <popo124117@gmail.com>
2020-04-13 11:34:22 +0800

    feat: [通訊錄資料表]

---
+++ app/src/main/java/com/augus/roomdeepresearch/Database/bean/AddressBook.kt
@@ -0,0 +1,100 @@
+package com.augus.roomdeepresearch.Database.bean
+
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.room.*
+import com.augus.roomdeepresearch.base.DatabaseConstants
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = DatabaseConstants.ADDRESS_BOOK_TABLE_NAME)
+data class AddressBook(
+
+    @PrimaryKey
+    @SerializedName("id")
+    @ColumnInfo(name = DatabaseConstants.ID)
+    val id: Int,
+
+    @SerializedName("conversationID")
+    @ColumnInfo(name = DatabaseConstants.CONVERSATION_ID)
+    val conversationID: String?,
+
+    @SerializedName("nickname")
+    @ColumnInfo(name = DatabaseConstants.NICKNAME)
+    val nickname: String?,
+
+    @SerializedName("avatar")
+    @Embedded
+    val avatar: Avatar?,
+
+    @SerializedName("appID")
+    @ColumnInfo(name = DatabaseConstants.APP_ID)
+    val appID: String?,
+
+    @SerializedName("gender")
+    @ColumnInfo(name = DatabaseConstants.GENDER)
+    val gender: String?,
+
+    @SerializedName("relation")
+    @ColumnInfo(name = DatabaseConstants.RELATION)
+    val relation: Int,
+
+    @SerializedName("notice")
+    @ColumnInfo(name = DatabaseConstants.NOTICE)
+    val notice: Boolean,
+
+    @PrimaryKey
+    @SerializedName("owner")
+    @ColumnInfo(name = DatabaseConstants.OWNER)
+    val owner: Int,
+
+    /**
+     * todo  需額外建立 聊天室列表資料表 chat_room_list 結構
+     */
+    @SerializedName("sticky")
+    @Ignore
+    val sticky: Boolean
+) : IItemLayoutRes, Parcelable {
+    override val layoutRes: Int
+        get() = 1
+
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readParcelable(Avatar::class.java.classLoader),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readInt(),
+        parcel.readByte() != 0.toByte(),
+        parcel.readInt(),
+        parcel.readByte() != 0.toByte()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(conversationID)
+        parcel.writeString(nickname)
+        parcel.writeParcelable(avatar, flags)
+        parcel.writeString(appID)
+        parcel.writeString(gender)
+        parcel.writeInt(relation)
+        parcel.writeByte(if (notice) 1 else 0)
+        parcel.writeInt(owner)
+        parcel.writeByte(if (sticky) 1 else 0)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<AddressBook> {
+        override fun createFromParcel(parcel: Parcel): AddressBook {
+            return AddressBook(parcel)
+        }
+
+        override fun newArray(size: Int): Array<AddressBook?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
---
+++ app/src/main/java/com/augus/roomdeepresearch/Database/bean/IItemLayoutRes.kt
@@ -0,0 +1,12 @@
+package com.augus.roomdeepresearch.Database.bean
+
+
+/**
+ * @author augus
+ * @create 2020-03-28
+ * @Describe
+ */
+interface IItemLayoutRes {
+    val layoutRes: Int
+}
+
