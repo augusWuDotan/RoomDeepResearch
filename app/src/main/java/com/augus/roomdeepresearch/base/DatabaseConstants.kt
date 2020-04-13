0fd2f1a8bbc6b3876df0e97b22b35a8265e7a1b2
augus_0331 <popo124117@gmail.com>
2020-04-13 10:45:05 +0800

    feat: 第一版 確認需要引用的套件 [Room][羅馬拼音-專案需求] 建立資料庫需求常數表 [DatabaseConstants]

---
+++ app/src/main/java/com/augus/roomdeepresearch/base/DatabaseConstants.kt
@@ -0,0 +1,60 @@
+package com.augus.roomdeepresearch.base
+
+class DatabaseConstants {
+    companion object {
+        /**
+         * 資料庫命名
+         */
+        const val SQL_NAME = "TfMessage.db"
+
+        /**
+         * 資料表命名
+         */
+        //通訊錄
+        const val ADDRESS_BOOK_TABLE_NAME = "address_book"
+
+        //新朋友
+        const val NEW_FRIEND_TABLE_NAME = "new_friend"
+
+        //群組
+        const val GROUP_LIST_TABLE_NAME = "group_list"
+
+        //聊天室
+        const val CHAT_ROOM_LIST_TABLE_NAME = "chat_room_list"
+
+        //聊天信息
+        const val CHAT_LIST_TABLE_NAME = "chat_list"
+
+        //官方帳號
+        const val OFFICIAL_ACCOUNT_TABLE_NAME = "official_account"
+
+        //紅包紀錄
+        const val RED_ENVELOPE_RECORD_TABLE_NAME = "red_envelope_record"
+
+        /**
+         * 欄位命名
+         */
+        const val ID = "id"
+        const val CONVERSATION_ID = "conversation_id"
+        const val NICKNAME = "nickname"
+        const val AVATAR_URL = "avatar_url"
+        const val AVATAR_TOKEN = "avatar_token"
+        const val APP_ID = "app_id"
+        const val GENDER = "gender"
+        const val RELATION = "relation"
+        const val NOTICE = "notice"
+        const val OWNER = "owner"
+        const val INTRODUCTION = "introduction"
+        const val IS_READ = "is_read"
+        const val CREATE_AT = "create_at"
+        const val NAME = "name"
+        const val IS_ANNOUNCEMENT = "is_announcement"
+        const val ANNOUNCEMENT = "announcement"
+        const val ANNOUNCER = "announcer"
+        const val ANNOUNCE_AT = "announce_at"
+        const val DISPLAY = "display"
+        const val FORBID_SPEAK = "forbid_speak"
+        const val ROLE = "role"
+        const val STATUS = "status"
+    }
+}
