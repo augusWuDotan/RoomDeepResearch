b373949de08246bbb0a4a7b0a47ea4f024772d24
augus_0331 <popo124117@gmail.com>
2020-04-15 10:27:40 +0800

    feat: 第一支DAO測試預備

--- app/src/main/java/com/augus/roomdeepresearch/database/TFDatabase.kt
+++ app/src/main/java/com/augus/roomdeepresearch/database/TFDatabase.kt
@@ -1,19 +1,24 @@
 package com.augus.roomdeepresearch.database
 
+import android.util.Log
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.augus.roomdeepresearch.base.BaseApplication
+import com.augus.roomdeepresearch.base.DatabaseConstants
 import com.augus.roomdeepresearch.database.bean.AddressBook
 import com.augus.roomdeepresearch.database.bean.Group
 import com.augus.roomdeepresearch.database.bean.NewFriend
-import com.augus.roomdeepresearch.base.BaseApplication
-import com.augus.roomdeepresearch.base.DatabaseConstants
+import com.augus.roomdeepresearch.database.dao.AddressBookDAO
 
+//
 @Database(
     entities = [AddressBook::class, NewFriend::class, Group::class],
     version = 1
 )
 abstract class TFDatabase : RoomDatabase() {
+
+    abstract val addressBook: AddressBookDAO
 
     companion object {
         val instance = TFDatabaseHolder.getDatabase()
@@ -21,6 +26,7 @@ abstract class TFDatabase : RoomDatabase() {
 
     object TFDatabaseHolder {
         fun getDatabase(): TFDatabase {
+            Log.d("TFDatabaseHolder", "getDatabase")
             return Room.databaseBuilder(
                     BaseApplication.context(),
                     TFDatabase::class.java,
@@ -28,7 +34,6 @@ abstract class TFDatabase : RoomDatabase() {
                 )
                 .build()
         }
-
 
     }
 }
---
+++ app/src/main/java/com/augus/roomdeepresearch/database/dao/AddressBookDAO.kt
@@ -0,0 +1,46 @@
+package com.augus.roomdeepresearch.database.dao
+
+import androidx.room.*
+import com.augus.roomdeepresearch.base.DatabaseConstants
+import com.augus.roomdeepresearch.database.bean.AddressBook
+import io.reactivex.Flowable
+import io.reactivex.Maybe
+import io.reactivex.Single
+
+@Dao
+interface AddressBookDAO {
+
+    @Insert
+    fun insertAddressBook(addressBook: AddressBook)
+
+    @Insert
+    fun insertAddressBooks(vararg addressBook: AddressBook): Maybe<MutableList<Long>>
+
+    //both 無法回傳
+    @Insert
+    fun insertBothAddressBooks(
+        addressBook1: AddressBook,
+        addressBook2: AddressBook
+    )
+
+    @Insert
+    fun insertAddressBooks(addressBooks: MutableList<AddressBook>): Maybe<MutableList<Long>>
+
+    @Update
+    fun updateAddressBook(addressBook: AddressBook): Single<Int>
+
+    @Update
+    fun updateAddressBooks(vararg addressBook: AddressBook): Single<Int>
+
+    @Update
+    fun updateAddressBooks(addressBooks: MutableList<AddressBook>): Single<Int>
+
+    @Delete
+    fun deleteAddressBooks(vararg addressBook: AddressBook): Single<Int>
+
+    @Query("SELECT * FROM ${DatabaseConstants.ADDRESS_BOOK_TABLE_NAME}")
+    fun getAddressBooks(): Flowable<MutableList<AddressBook>>
+
+    @Query("SELECT * FROM ${DatabaseConstants.ADDRESS_BOOK_TABLE_NAME}")
+    fun getAddressBooksSortWithRomanPinyin(): Flowable<MutableList<AddressBook>>
+}
