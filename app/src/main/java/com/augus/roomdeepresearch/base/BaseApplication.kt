b2772566e6e617f4163b4331e574d36a02c8b591
augus_0331 <popo124117@gmail.com>
2020-04-15 09:40:11 +0800

    feat: 資料庫初始化時機

--- app/src/main/java/com/augus/roomdeepresearch/base/BaseApplication.kt
+++ app/src/main/java/com/augus/roomdeepresearch/base/BaseApplication.kt
@@ -2,6 +2,7 @@ package com.augus.roomdeepresearch.base
 
 import android.app.Application
 import android.content.res.Configuration
+import com.augus.roomdeepresearch.database.TFDatabase
 
 class BaseApplication : Application() {
 
@@ -15,6 +16,12 @@ class BaseApplication : Application() {
         fun context(): BaseApplication {
             return context as BaseApplication
         }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        //init
+        TFDatabase.instance
     }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
